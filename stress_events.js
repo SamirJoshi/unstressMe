e44fb3a0e37c9ffb8ffff1c5e6be2e6e06f6bcf9
SamirJoshi <samirjoshi2018@u.northwestern.edu>
2017-02-13 04:06:08 -0600

    show json events for both stress and decision;

---
+++ stress_events.js
@@ -0,0 +1,20 @@
+[
+  {
+    "type": "stress",
+    "name": "Got my midterm back yesterday",
+    "stressLevel": "20",
+    "fact": "I did well",
+    "feelings": "I'm happy",
+    "fiction": "I'll do well on every test",
+    "timestamp": "none"
+  },
+  {
+    "type": "stress",
+    "name": "Idk",
+    "stressLevel": "100",
+    "fact": "I did well",
+    "feelings": "I'm happy",
+    "fiction": "I'll do well on every test",
+    "timestamp": "none"
+  },
+]
