99e2e56eba483009d50de46df471f124bc97a159
mniemer <matthewniemer2018@u.northwestern.edu>
2017-02-13 08:26:47 -0600

    minor changes

--- unstressMe.js
+++ unstressMe.js
@@ -1,5 +1,12 @@
-
-
 function HelloWorld(){
   
 }
+
+function WriteToFile() {
+
+    var fso = CreateObject("Scripting.FileSystemObject");  
+    var s = fso.CreateTextFile("C:\\Test.txt", true);
+    var text=document.getElementById("stressForm").innerText;
+    s.Close();
+ }
+
